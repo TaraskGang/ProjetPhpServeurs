89e3ebf60bb6ff6248512852c9ec075c3155283b
TaraskGang <angelus.ganga@gmail.com>
2018-07-05 11:43:25 +0200

    commit all

---
+++ public-html/index.php
@@ -0,0 +1,15 @@
+﻿
+<?php
+$title = 'Site d\'exemple 1';
+?>
+<!Doctype>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title><?= $title ?></title>
+    </head>
+    
+    <body>
+    <h1><?= $title ?></h1>
+    </body>
+</html>
